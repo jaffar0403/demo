28b1a920d9a239c1a2b749920c68a8929391efd3
jack <jack@123>
2024-10-29 20:33:12 +0000

    First commit from jack

--- test.py
+++ test.py
@@ -11,13 +11,23 @@ class Dog:
     def get_age(self):
         # Method to get the age of the dog
         return f"{self.name} is {self.age} years old."
+    def demo(self):
+        print("demo testing")
+
+    def age(self):
+        print(self.age)
+
+
+
 
 # Create an instance of the Dog class
 my_dog = Dog("Buddy", 3)
 d=Dog("remo",3)
+#d.age()
 
 # Call the methods
 print(my_dog.bark())       # Output: Buddy says woof!
 print(my_dog.get_age())    # Output: Buddy is 3 years old.
 print(d.get_age())
+d.demo()
 
